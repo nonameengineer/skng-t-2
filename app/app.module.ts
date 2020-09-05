9b65b6faaf6eb90353ec80539e245db523992d38
nonameengineer <ruslanusm6@gmail.com>
2020-09-05 18:46:26 +0500

    1 file modified

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,14 +1,21 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { FormsModule } from "@angular/forms";
 
-import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
-import { TestComponent } from './test.component';
+import { AppComponent } from "./app.component";
+import { HelloComponent } from "./hello.component";
+import { TestComponent } from "./test.component";
+import { ViewportModule } from "./viewport/viewport.module";
+import { IConfig } from "./viewport/models/config.interface";
+
+const config: IConfig = {
+  medium: 720,
+  large: 1240
+};
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, TestComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [BrowserModule, FormsModule, ViewportModule.forRoot(config)],
+  declarations: [AppComponent, HelloComponent, TestComponent],
+  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
